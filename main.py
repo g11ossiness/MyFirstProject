5f4db9e9b32322ac6a7f9578a2644414e62522d4
Anasthesia <g11ossiness@gmail.com>
2026-01-09 00:25:06 +0900

    Faker в experiment

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-# main.py
-def sum(a, b):
-    return a + b
+﻿# использование библиотеки Faker
+from faker import Faker
 
-# певрый код =)
-print("МОЯ ПЕРВАЯ ПРОГРАММА =)")
+fake = Faker('ru_RU')
 
-x = 5
-y = 10
-total = sum(x, y)  # доступно
-print("СУММА:", total)
-
-# requests 
-import requests
-response = requests.get("https://httpbin.org/get")
-print("СТАТУС ОТВЕТА:", response.status_code)
-print("ДЛИНА ОТВЕТА:", len(response.text))
-
-import datetime
-
-print("=" * 40)
-print("СЕЙЧАС:", datetime.datetime.now().strftime("%d.%m.%Y %H:%M"))
-print("=" * 40)
+print("имя:", fake.name())
+print("адрес:", fake.address())
+print("email:", fake.email())
