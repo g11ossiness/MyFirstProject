9bb073a4c5597af5e90892b72573ebe1b3b52efc
Anasthesia <g11ossiness@gmail.com>
2026-01-08 23:56:12 +0900

    сделала отображаемый текст капсом

--- main.py
+++ main.py
@@ -2,22 +2,22 @@
 def sum(a, b):
     return a + b
 
-# певрый код
-print("моя первая программа =)")
+# певрый код =)
+print("МОЯ ПЕРВАЯ ПРОГРАММА =)")
 
 x = 5
 y = 10
 total = sum(x, y)  # доступно
-print("cумма:", total)
+print("СУММА:", total)
 
 # requests 
 import requests
 response = requests.get("https://httpbin.org/get")
-print("cтатус код ответа:", response.status_code)
-print("длина текста ответа:", len(response.text))
+print("СТАТУС ОТВЕТА:", response.status_code)
+print("ДЛИНА ОТВЕТА:", len(response.text))
 
 import datetime
 
 print("=" * 40)
-print("сейчас:", datetime.datetime.now().strftime("%d.%m.%Y %H:%M"))
+print("СЕЙЧАС:", datetime.datetime.now().strftime("%d.%m.%Y %H:%M"))
 print("=" * 40)
