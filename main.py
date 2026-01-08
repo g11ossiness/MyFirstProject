a92497b7a38670a71882781b9e040d9a88ddb260
Anasthesia <g11ossiness@gmail.com>
2026-01-08 23:38:03 +0900

    обновленный код

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ print("моя первая программа =)")
 x = 5
 y = 10
 total = sum(x, y)  # доступно
-print("Сумма:", total)
+print("cумма:", total)
 
 # requests 
 import requests
 response = requests.get("https://httpbin.org/get")
-print("Статус код ответа:", response.status_code)
-print("Длина текста ответа:", len(response.text))
+print("cтатус код ответа:", response.status_code)
+print("длина текста ответа:", len(response.text))
 
 import datetime
 
 print("=" * 40)
-print("Запуск программы:", datetime.datetime.now().strftime("%d.%m.%Y %H:%M"))
+print("сейчас:", datetime.datetime.now().strftime("%d.%m.%Y %H:%M"))
 print("=" * 40)
