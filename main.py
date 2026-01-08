527d213d470ef69ba4269791d18c26afd4ddb0ef
Anasthesia <g11ossiness@gmail.com>
2026-01-08 23:01:22 +0900

    вывод текущей даты

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ import requests
 response = requests.get("https://httpbin.org/get")
 print("Статус код ответа:", response.status_code)
 print("Длина текста ответа:", len(response.text))
+
+import datetime
+
+print("=" * 40)
+print("Запуск программы:", datetime.datetime.now().strftime("%d.%m.%Y %H:%M"))
+print("=" * 40)
