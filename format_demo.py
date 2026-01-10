352d9224464e77b870e4244f043c42f5c4f084e6
Anasthesia <g11ossiness@gmail.com>
2026-01-10 22:46:08 +0900

    задание 7, 09.12 - исправлены отступы в main.py - создан файл format_demo.py с примером плохого форматирования - протестировала autopep8

---
+++ format_demo.py
@@ -0,0 +1,13 @@
+# задание 7
+def sum_numbers(a, b):
+    return a+b  # нет пробелов
+
+
+first_number = 5
+second_number = 10
+result = sum_numbers(first_number, second_number)
+print("результат:", result)
+
+my_list = [1, 2, 3, 4, 5]
+for i in my_list:
+    print(i*2)
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-﻿# использование библиотеки Faker
-from faker import Faker
+﻿# TODO: проверка на пустой ввод
+# main.py
+import datetime
+import requests
+def sum(a, b):
+    return a + b
 
-fake = Faker('ru_RU')
+# мой первый код
+print("моя первая программа =)")
 
-print("имя:", fake.name())
-print("адрес:", fake.address())
-print("email:", fake.email())
+x = 5
+y = 10
+total = sum(x, y)  # доступно
+print("cумма:", total)
+
+# requests
+response = requests.get("https://httpbin.org/get")
+print("cтатус код ответа:", response.status_code)
+print("длина текста ответа:", len(response.text))
+
+
+print("=" * 40)
+print("сейчас:", datetime.datetime.now().strftime("%d.%m.%Y %H:%M"))
+print("=" * 40)
